038b8a1ae61fed5c5ece0e728b0cb39003922392
byan23 <byan23@wisc.edu>
2015-09-27 16:07:52 -0500

    9/27 Refactorized...

--- linux/mysh.c
+++ linux/mysh.c
@@ -7,43 +7,49 @@
 #include <sys/wait.h>
 
 #define MAX_CLINE 512
-
+// whitespace
+#define WS " \n\t"
 // TODO(byan23): Maybe add reallocation for cases larger than MAX_TOKEN or
 // eventially come up with a dynamical way to allocate the mem.
 #define MAX_TOKEN 50
 #define HISTORY_POOL_SIZE 20
 
-typedef enum {
-  EXIT_MODE	    = 1;
-  HISTORY_MODE	    = 2;
-  RUN_HISTORY_MODE  = 3;
-  RUN_BIN_MODE	    = 4;
-  SYNTAX_ERR	    = 5;
+typedef enum CMD_MODE {
+  EXIT_MODE	    = 1,
+  HISTORY_MODE	    = 2,
+  RUN_HISTORY_MODE  = 3,
+  RUN_BIN_MODE	    = 4,
+  SYN_ERR	    = 5,
+  NULL_MODE	    = 6
 } CMD_MODE;
 
+// whatever error...
+static char error_message[30] = "An error has occurred\n";
+#define err_len 30
 // The list of history cmds.
 char *his_list[HISTORY_POOL_SIZE];
-int his_num = 0;
+static int his_num = 0;
 // current latest cmd index
-int oldest_idx = 0;
+static int oldest_idx = 0;
 
 // Function for built-in cmd 'exit'.
 void bin_exit();
 // Function for built-in cmd 'history'.
 void bin_history();
 // Function for '!' cmd.
-void run_his_cmd(int num);
+void run_his_cmd(char *str);
 // Function to parse and exec built-in cmd.
 void exec_cmd(char *str);
 
-// helper functions that may be deleted (sadly) later...
+// helper functions that (some) may be deleted (sadly) later...
 void print_args(char *args[]);
+void add_to_history(const char *str);
 int token_num(char *str, char c);
-// Use the 1st token to get the run mode.
-CMD_MODE get_mode(char *str);
+// Passes in the whole cmd string, returns status including syntax_err mode.
+// TODO(byan23): Try to combine checking mode and tokenizing.
+CMD_MODE get_mode(const char *s);
 
 
-// TODO(byan23): Decide whether or not to keep "/bin/" prefix.
 // Main for shell, syntax errors should be checked before calling any
 // command.
 int main(int argc, char *argv[]) {
@@ -52,84 +58,41 @@ int main(int argc, char *argv[]) {
     printf("mysh # ");
     memset(rc_str, 0, MAX_CLINE);
     fgets(rc_str, MAX_CLINE, stdin);
-    char *cmd = strdup(rc_str);
-    // Adds into history list, increments 'his_num' if it's not yet full.
-    if (his_num < HISTORY_POOL_SIZE) {
-      his_list[his_num] = cmd;
-      ++his_num;
-    } else {
-      his_list[oldest_idx] = cmd;
-      oldest_idx = (oldest_idx + 1) % HISTORY_POOL_SIZE;
-    }
-    char *pch = strtok(rc_str, " \n\t");
-    if (pch != NULL) {
-      //int num_of_token = token_num(rc_str, pch[0]);
-      //char *rc_argv[num_of_token + 1];
-      // Switch mode.
-      CMD_MODE mode = get_mode(pch);
-      if (mode == 1) bin_exit();
-      else {
-	// Parse the cmd.
-	char *rc_argv[MAX_TOKEN];
-	rc_argv[0] = strdup("/bin/");
-	strcat(rc_argv[0], pch);
-	int t_idx = 1;
-	//printf("%d tokens.\n", num_of_token);
-	while ((pch = strtok(NULL, " \n\t")) != NULL) {
-	  //assert(t_idx < num_of_token);
-	  rc_argv[t_idx] = strdup(pch);
-	  ++t_idx;
-	}   
-	rc_argv[t_idx] = NULL;	
-	int rc = fork();
-	if (rc == 0) {
-	  // child
+    // Switch mode.
+    CMD_MODE mode = get_mode(rc_str);
+    printf("Got mode: %d\n", mode); 
+    if (mode != NULL_MODE) {
+      if (mode == SYN_ERR) {
+	printf("Syntax error!\n");
+	write(STDERR_FILENO, error_message, err_len);
+      } else {
+	// Adds to history.
+	printf("Adding cmd to history before doing anything.\n");
+	add_to_history(rc_str);
+	if (mode == EXIT_MODE) {
+	  bin_exit();
+	} else {
+	  // Parse the cmd.
+	  // TODO(byan23): Made it a dynamically allocated variable.
+	  printf("a mode we need to do something...\n");
 	  switch (mode) {
-	    case 2:
+	    case HISTORY_MODE:
+	      printf("his mode\n");
 	      bin_history();
 	      break;
-	    case 3:
-	      run_his_cmd();
+	    case RUN_HISTORY_MODE:
+	      printf("run his mode\n");
+	      run_his_cmd(rc_str);
 	      break;
-	    case 4:
-
+	    case RUN_BIN_MODE:
+	      printf("I just parse and pass...\n");
+	      exec_cmd(rc_str);
+	      break;
+	    default:
+	      printf("Unexpected mode: %d\n", mode);
+	      break;
 	  }
-	} else if (rc > 0) {
-	  // parent
-	  wait(NULL);
-	  //printf("Parent process: %d\n", (int) getpid());
-	} else {
-	  // failure
-	  perror("Fork failure.\n");
 	}
-      }
-      /*char *rc_argv[MAX_TOKEN];
-      rc_argv[0] = strdup("/bin/");
-      strcat(rc_argv[0], pch);
-      int t_idx = 1;
-      while ((pch = strtok(NULL, " \n\t")) != NULL) {
-	rc_argv[t_idx] = strdup(pch);
-	++t_idx;
-      }
-      rc_argv[t_idx] = NULL;*/
-      //print_args(rc_argv);
-      //char *rcargv[] = {"ls", "../", NULL};
-      //assert(t_idx == num_of_token);
-      int rc = fork();
-      if (rc == 0) {
-	// child
-	//printf("Child process: %d\n", (int) getpid());
-	//printf("Exec command: %s\n", rc_argv[0]);
-	execvp(rc_argv[0], rc_argv);
-	perror("Exec failure!\n");
-	bin_exit();
-      } else if (rc > 0){
-	// parent
-	wait(NULL);
-	//printf("Parent process: %d\n", (int) getpid());
-      } else {
-	// failure
-	perror("Fork failure!\n");
       }
     }
   }
@@ -140,20 +103,43 @@ void bin_exit() {
   exit(0);
 }
 
-// TODO(byan23): Parsing finished. Finish exec part.
+// TODO(byan23): Dynamically allocate char *argv[].
 void exec_cmd(char *str) {
   char *rc_argv[MAX_TOKEN];
+  str = strtok(str, WS);
   rc_argv[0] = strdup("/bin/");
-  strcat(rc_argv[0], pch);
+  strcat(rc_argv[0], str);
   int t_idx = 1;
-  while ((pch = strtok(NULL, " \n\t")) != NULL) {
-  rc_argv[t_idx] = strdup(pch);
+  while ((str = strtok(NULL, WS)) != NULL) {
+    rc_argv[t_idx] = strdup(str);
     ++t_idx;
   }
   rc_argv[t_idx] = NULL;
+  print_args(rc_argv);
+  int rc = fork();
+  if (rc == 0) {
+    // child
+    execvp(rc_argv[0], rc_argv);
+    perror("Exec failure.\n");
+    bin_exit();
+  } else if (rc > 0) {
+    // parent
+    wait(NULL);
+    printf("Parent process: %d\n", (int) getpid());
+  } else {
+    // failure
+    perror("Fork failure.\n");
+  }
 }
 
 // TODO(byan23): Implement history built-in cmd.
+void bin_history() {
+  return;
+}
+
+void run_his_cmd(char *str) {
+  return;
+}
 
 // It is caller's responsibility to ensure 'str' is NOT null and 'c' is the
 // 1st non-whitespace character.
@@ -165,11 +151,23 @@ int token_num(char *str, char c) {
   printf("local string: %s\n", p);
   int count = 1;  // 'str' is already pointing at the beginning of 1st token
   ++p;
-  while ((p = strchr(p, ' ')) != NULL) {
+  while (!(p = strchr(p, ' '))) {
     ++p;
     ++count;
   }
   return count;
+}
+
+void add_to_history(const char *str) {
+  if (his_num < HISTORY_POOL_SIZE) {
+    // Appends new cmd at the end of history array.
+    printf("History pool is not full yet.\n");
+    his_list[his_num++] = strdup(str);
+    /* oldest_idx stay at 0 */
+  } else {
+    strcpy(his_list[oldest_idx], str);
+    oldest_idx = (oldest_idx + 1) % HISTORY_POOL_SIZE;
+  }
 }
 
 void print_args(char *args[]) {
@@ -181,19 +179,46 @@ void print_args(char *args[]) {
   printf("\n");
 }
 
-CMD_MODE get_mode(char *str) {
-  if (strncmp(str, "exit", 4) == 0) {
-    if (str[4] == '\0') return CMD_MODE.EXIT_MODE;
-    else return CMD_MODE.SYNTAX_ERR;
-  } else if {strncmp(str, "history", 7) == 0} {
-    if (str[7] == '\0') return CMD_MODE.HISTORY_MODE;
-    else return CMD_MODE.SYNTAX_ERR;
-  } else if (str[0] == '!') {
-    char *p;
-    long temp = strtol(&str[1], &p, 10);
-    if (*p == '\0' || str[1] == '\0') return CMD_MODE.RUN_HISTORY_MODE;
-    else return CMD_MODE.SYNTAX_ERR;
+CMD_MODE get_mode(const char *s) {
+  // Works on a copy, since strtok changes the original string.
+  printf("Getting mode...\n");
+  char str[strlen(s) + 1];
+  strcpy(str, s);
+  printf("made a copy...\n");
+  char *p;
+  if ((p = strstr(str, ">"))) {
+    printf("command needs redirection...\n");
+    // can only have one '>'.
+    if (strchr(++p, '>')) return SYN_ERR;
+    // Checks if more than 1 arg appear after '>'.
+    char str_cpy[strlen(str) + 1];
+    strcpy(str_cpy, str);
+    char *start = strchr(str_cpy, '>');
+    start = strtok(++start, WS);
+    if (strtok(NULL, WS)) {
+      printf("Invalid command: %s\n", str);
+      return SYN_ERR;
+    }
+  }
+  p = strtok(str, WS);
+  printf("1st token: %s\n", p);
+  if (!p) return NULL_MODE;	// empty command
+  if (p[0] == '>') {
+    return SYN_ERR;
+  } else if (p[0] == '!') {
+    char *pleft;
+    long temp = strtol(&p[1], &pleft, 10);
+    printf("Use it since you are not happy...%lu\n", temp);
+    if (*pleft == '\0' || p[1] == '\0') return RUN_HISTORY_MODE;
+    else return SYN_ERR;
+  } else if (strlen(p) > 4 && strncmp(p, "exit", 4) == 0) {
+    if (p[4] == '\0' && !strtok(NULL, WS)) return EXIT_MODE;
+    else return SYN_ERR;
+  } else if (strlen(p) > 7 && strncmp(p, "history", 7) == 0) {
+    if (p[7] == '\0' && !strtok(NULL, WS)) return HISTORY_MODE;
+    else return SYN_ERR;
   } else {
-    return CMD_MODE.RUN_BIN_MODE;
+    printf("built-in mode\n");
+    return RUN_BIN_MODE;
   }
 }
